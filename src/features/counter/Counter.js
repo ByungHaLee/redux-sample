5c82103f5ecdf82dc536cace0d7f779232e76131
Byung Ha Lee <byungha.lee@metronom.com>
2020-08-20 12:06:12 +0200

    Change function to class

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,60 +1,83 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import {
   decrement,
   increment,
   incrementByAmount,
   incrementAsync,
-  selectCount,
 } from './counterSlice';
 import styles from './Counter.module.css';
+import {connect} from "react-redux";
 
-export function Counter() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+class Counter extends React.Component{
 
-  return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
+  constructor(props) {
+    super(props);
+    this.state = {
+      incrementAmount:2
+    };
+  }
+  setIncrementAmount(value) {
+    this.setState({incrementAmount:value})
+  }
+  render() {
+    const {incrementAmount} = this.state
+    const {count, increment, decrement, incrementByAmount, incrementAsync} = this.props
+    return (
+      <div>
+        <div className={styles.row}>
+          <button
+            className={styles.button}
+            aria-label="Increment value"
+            onClick={() => increment()}
+          >
+            +
+          </button>
+          <span className={styles.value}>{count}</span>
+          <button
+            className={styles.button}
+            aria-label="Decrement value"
+            onClick={() => decrement()}
+          >
+            -
+          </button>
+        </div>
+        <div className={styles.row}>
+          <input
+            className={styles.textbox}
+            aria-label="Set increment amount"
+            value={incrementAmount}
+            onChange={e => this.setIncrementAmount(e.target.value)}
+          />
+          <button
+            className={styles.button}
+            onClick={() =>
+              incrementByAmount(Number(incrementAmount) || 0)
+            }
+          >
+            Add Amount
+          </button>
+          <button
+            className={styles.asyncButton}
+            onClick={() => incrementAsync(Number(incrementAmount) || 0)}
+          >
+            Add Async
+          </button>
+        </div>
       </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={e => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() =>
-            dispatch(incrementByAmount(Number(incrementAmount) || 0))
-          }
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
-        >
-          Add Async
-        </button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
+const mapStateToProps = state => {
+  const { counter } = state
+  const count = counter.value
+  return { count }
+}
+export default connect(mapStateToProps,
+    {
+      decrement,
+      increment,
+      incrementByAmount,
+      incrementAsync
+    }
+)(Counter)
